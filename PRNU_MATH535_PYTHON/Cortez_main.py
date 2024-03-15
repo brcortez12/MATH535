4ea9b4998b8dfd66ffb3bf0ed10b9c9f7b99f80c
Brandon Cortez <brandonrcortez12@gmail.com>
2024-03-15 14:46:06 -0500

    Completed correlation functions to return accurate match results
    - Optimized Image Transform functions to reduce redundant print statements and consolidate repeated code
    - Completed Correlation and match functions to return correct results
    - Code still needs to be modified to use naming specified in requirements document and output requested analysis but getting close
    - Added provided "other" images from different camera

--- PRNU_MATH535_PYTHON/Cortez_main.py
+++ PRNU_MATH535_PYTHON/Cortez_main.py
@@ -7,46 +7,24 @@ from src.Filter import NoiseExtractFromImage
 
 def create_camera_fingerprint(image_folder):
     image_paths = [os.path.join(image_folder, img) for img in os.listdir(image_folder)]
-    image_paths.sort()
     image_paths = image_paths[:30]
     camera_fingerprint, _ = getFingerprint(image_paths)
     return camera_fingerprint
-
-def extract_image_fingerprint(image_path):
-    image_fingerprint = NoiseExtractFromImage(image_path, sigma=2.)
-    return image_fingerprint
-
-def calculate_correlation(camera_fingerprint, image_fingerprint):
-    # Calculates the correlation between camera and image fingerprints.
-    # Flatten the fingerprints to 1D arrays
-    camera_fingerprint_flat = camera_fingerprint.flatten()
-    image_fingerprint_flat = image_fingerprint.flatten()
-    # Calculate the correlation coefficient
-    correlation_matrix = np.corrcoef(camera_fingerprint_flat, image_fingerprint_flat)
-    # The correlation coefficient between camera and image fingerprints is at position (0, 1) of the matrix
-    correlation_value = correlation_matrix[0, 1]
-
-    return correlation_value
-
-def decide_match(correlation_value, threshold):
-    match = correlation_value >= threshold
-    return match
 
 def main():
     camera_input_folder = 'PRNU_MATH535_PYTHON/Camera Fingerprint Images'
     other_input_folder = 'PRNU_MATH535_PYTHON/Other Images'
     output_folder = 'PRNU_MATH535_PYTHON'
 
-    print("Preprocessing images...")
     process_images(camera_input_folder, other_input_folder, output_folder)
-    print("Images preprocessed.")
 
     print("Creating camera fingerprint...")
-    camera_fingerprint = create_camera_fingerprint(os.path.join(output_folder, 'Camera Fingerprint Images Grey Cropped'))
+    PRNU_Ref_Image = create_camera_fingerprint(os.path.join(output_folder, 'Camera Fingerprint Images Grey Cropped'))
+    cv2.imwrite('PRNU_MATH535_PYTHON\PRNU_Ref_Image.jpeg', PRNU_Ref_Image)
     print("Camera fingerprint created.")
 
     test_image_path = os.path.join(output_folder, 'Other Images Grey Cropped', 'IMG_1736.jpeg')
-    test_fingerprint = extract_image_fingerprint(test_image_path)
+    test_fingerprint = NoiseExtractFromImage(test_image_path)
 
     # Save the fingerprint as ImageFPrint(i)
     output_file_path = os.path.join(output_folder, 'ImageFPrint(i).txt')
@@ -59,14 +37,17 @@ def main():
     for image_name in os.listdir(os.path.join(output_folder, 'Other Images Grey Cropped')):
         image_path = os.path.join(output_folder, 'Other Images Grey Cropped', image_name)
         print(f"Processing image: {image_name}")
-        image_fingerprint = extract_image_fingerprint(image_path)
+        image_fingerprint = NoiseExtractFromImage(image_path)
         
         # Calculate correlation
-        correlation_value = calculate_correlation(camera_fingerprint, image_fingerprint)
-
+        # Compute the element-wise product of U and A, then flatten the result
+        UA_flat = (PRNU_Ref_Image * image_fingerprint).flatten()
+        # Flatten the image fingerprint
+        B_flat = image_fingerprint.flatten()
+        corrvalue = 1 - np.corrcoef(UA_flat, B_flat)[0, 1]
         threshold = 0.6
-        match = decide_match(correlation_value, threshold)
-        print(f"Image: {image_name}, Correlation: {correlation_value}, Match: {match}")
+        match = corrvalue >= threshold
+        print(f"Image: {image_name}, Correlation: {corrvalue}, Match: {match}")
 
 if __name__ == "__main__":
     main()
--- PRNU_MATH535_PYTHON/Transform_Images.py
+++ PRNU_MATH535_PYTHON/Transform_Images.py
@@ -3,15 +3,18 @@ import os
 import cv2
 import numpy as np
 
-def center_crop(image):
-    h, w = image.shape[:2]
-    size = min(h, w)
-    top = (h - size) // 2
-    bottom = top + size
-    left = (w - size) // 2
-    right = left + size
-    cropped_image = image[top:bottom, left:right]
-    return cropped_image
+def crop_and_save(output_path, img_path):
+    print(f"Processing image: {img_path}")
+    img = cv2.imread(img_path)
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    h, w = grayscale_img.shape
+    startx = w//2 - (1024//2)
+    starty = h//2 - (1024//2)
+    if w != 1024 or h != 1024:
+        processed_img = grayscale_img[starty:starty+1024, startx:startx+1024]
+    else:
+        processed_img = grayscale_img
+    cv2.imwrite(output_path, processed_img)
 
 def process_images(camera_input_folder, other_input_folder, output_folder):
     print("Processing images...")
@@ -22,37 +25,14 @@ def process_images(camera_input_folder, other_input_folder, output_folder):
     os.makedirs(other_output_folder, exist_ok=True)
 
     # Process camera fingerprint images
-    print(f"Processing camera fingerprint images from: {camera_input_folder}")
     for img_name in os.listdir(camera_input_folder):
         img_path = os.path.join(camera_input_folder, img_name)
-        print(f"Processing image: {img_name}")
-        img = cv2.imread(img_path)
-        grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cropped_img = center_crop(grayscale_img)
         output_path = os.path.join(camera_output_folder, img_name)
-        cv2.imwrite(output_path, cropped_img)
-        print(f"Image processed and saved at: {output_path}")
-
+        crop_and_save(output_path, img_path)
+        
     # Process other images
-    print(f"Processing other images from: {other_input_folder}")
     for img_name in os.listdir(other_input_folder):
         img_path = os.path.join(other_input_folder, img_name)
-        print(f"Processing image: {img_name}")
-        img = cv2.imread(img_path)
-        grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cropped_img = center_crop(grayscale_img)
         output_path = os.path.join(other_output_folder, img_name)
-        cv2.imwrite(output_path, cropped_img)
-        print(f"Image processed and saved at: {output_path}")
-
-def main():
-    # Paths
-    camera_input_folder = 'PRNU_MATH535_PYTHON/Camera Fingerprint Images'
-    other_input_folder = 'PRNU_MATH535_PYTHON/Other Images'
-    output_folder = 'PRNU_MATH535_PYTHON'
-
-    # Process images
-    process_images(camera_input_folder, other_input_folder, output_folder)
-
-if __name__ == "__main__":
-    main()
+        crop_and_save(output_path, img_path)
+    print("Processing Done.")
